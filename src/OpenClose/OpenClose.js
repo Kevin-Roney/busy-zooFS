793801dee908a98a7bf04d28a9f4fbc6cf350b59
Kevin Roney <ktroney.pdx@gmail.com>
2022-04-28 15:53:38 -0700

    finished OpenClose and all is displaying properly

--- src/OpenClose/OpenClose.js
+++ src/OpenClose/OpenClose.js
@@ -2,7 +2,23 @@ import React, { useState } from 'react';
 
 export default function OpenClose() {
   const [isOpen, setIsOpen] = useState(false);
+  function handleOpenZoo() {
+    setIsOpen(true);
+  }
+  function handleCloseZoo() {
+    setIsOpen(false);
+  }
   return (
-    <div>isOpen</div>
+    <div>
+      <div>
+        {
+          isOpen
+            ? 'Welcome to the Oregon Zoon!' 
+            : 'Sorry we are closed, see you next time!'
+        }
+      </div>
+      <button onClick={handleOpenZoo}>☀️</button>
+      <button onClick={handleCloseZoo}>🌙</button>
+    </div>
   );
 }
