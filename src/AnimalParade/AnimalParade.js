3a01a642c070db00e816d6d9ade794bfde69b4f5
Kevin Roney <ktroney.pdx@gmail.com>
2022-04-28 16:46:57 -0700

    finished animal parade portion of the page, time for css *yayy..*

--- src/AnimalParade/AnimalParade.js
+++ src/AnimalParade/AnimalParade.js
@@ -2,8 +2,36 @@ import React, { useState } from 'react';
 import Animal from './Animal';
 
 export default function AnimalParade() {
-  const [animals, setAnimals] = useState([])
+  const [animals, setAnimals] = useState(['rat', 'rat', 'skunk']);
+
+  function handleAddRat() {
+    animals.push('rat');
+    setAnimals([...animals]);
+  }
+  function handleAddRaccoon() {
+    animals.push('raccoon');
+    setAnimals([...animals]);
+  }
+  function handleAddBat() {
+    animals.push('bat');
+    setAnimals([...animals]);
+  }
+  function handleAddSkunk() {
+    animals.push('skunk');
+    setAnimals([...animals]);
+  }
+
   return (
-    <div>AnimalParade</div>
+    <div className='parade'>
+      <div>
+        {
+          animals.map((animal) => <Animal key={animal} animal={animal}/>)
+        }
+      </div>
+      <button onClick={handleAddRat}>Oh hecc more Rats!</button>
+      <button onClick={handleAddRaccoon}>Oh hecc more Raccoons!</button>
+      <button onClick={handleAddBat}>Oh hecc more Bats!</button>
+      <button onClick={handleAddSkunk}>Oh hecc more Skunks!</button>
+    </div>
   );
 }
