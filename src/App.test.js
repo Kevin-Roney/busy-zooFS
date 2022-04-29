ffd15b97d0ab2b1fa1ebe118230c1a14fec47300
Kevin Roney <ktroney.pdx@gmail.com>
2022-04-28 17:51:47 -0700

    wrote second test just to try it out

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import FightClub from './FightClub/FightClub';
+import AnimalParade from './AnimalParade/AnimalParade';
 
-test('renders learn react link', () => {
+test('renders Welcome to the Sketchy Roadside Zoo! string', () => {
   render(<App />);
   const linkElement = screen.getByText(/Welcome to the Sketchy Roadside Zoo!/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders The Shark is charging up! button', () => {
+  render(<FightClub />);
+  const linkElement = screen.getByText(/The Shark is charging up!/i);
   expect(linkElement).toBeInTheDocument();
 });
